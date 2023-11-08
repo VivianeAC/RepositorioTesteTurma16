eda8d90066aa56782195032ae25a237e17fc18ed
Viviane <vivialmeidadecarvalho18@gmail.com>
2023-11-07 21:04:14 -0300

    PrimeiroCommit

---
+++ src/test/java/PrimeiroCodTurma17/PrimeiroCodTurma17.java
@@ -0,0 +1,45 @@
+package PrimeiroCodTurma17;
+
+public class PrimeiroCodTurma17 {
+
+	public static void main(String[] args) {
+		
+	    //Tipo  // nome    recebe      Valor
+		String    nome       =    "Viviane";   // Texto
+		String   sobrenome   =    "Carvalho";  // Texto
+		int      idade       =      47 ;       // numeros inteiros 
+		boolean   casada     =     true;       // ou False 
+		double    altura     =     1.65;       // Numeros Decimal
+		String   endereço     =    "Rua Ite";
+		int      numero      =         102 ;
+		String   cidade       =    "São Paulo " ;
+		String   estado       =    "São Paulo " ;
+		String   Pais        =    "Brasil " ;
+		double    CEP        =     03035-030 ;
+		String    mae       =     " Leonora" ;
+		String    pai       =      " Airton" ;
+		String   filhos     =      " Katherine , Kaliel , Hadassa ";
+	 
+		// imprimir informação 
+		
+		System.out.println("/n/n/n");
+	
+		System.out.println("Nome: "     +    nome);
+		System.out.println("Sobrenome  "  +  sobrenome);
+		System.out.println("Idade:"      +        idade);
+		System.out.println("Casada?:"    +       casada);
+		System.out.println("Altura:"     +       altura);
+		System.out.println("mae: "       +         mae);
+		System.out.println("pai:"        +        pai );
+		System.out.println("filhos:"     +     filhos   );
+		System.out.println("endereço:"   +      endereço);
+		System.out.println("cidade:"     +      cidade);
+		System.out.println("estado: "    +     estado);
+		System.out.println("Pais :"      +       Pais);
+		
+		
+		System.out.println("\n" + "Olá, meu nome é : "   +  nome      +    sobrenome     +  " tenho: " + idade + " anos: " + altura + " m .\n" + 
+		" meu endereço :"   +   endereço    +   numero    + " na cidade:"   +  cidade     +  "no estado:" +   estado    +    " no CEP:" + CEP   +   "no Pais:" + Pais  );
+	}
+
+}
